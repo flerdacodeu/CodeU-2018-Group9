1e35b8b655dd72970797023e5fa7669c74c2bf07
MennaMohie <mennaxmohiex@gmail.com>
2018-05-20 23:01:42 +0200

    Added second Follow-up (anagram of sentences)
    Finished Q1.

--- MennaMohie/assignment1/Q1.cpp
+++ MennaMohie/assignment1/Q1.cpp
@@ -1,9 +1,11 @@
 #include<bits/stdc++.h>
+#include<stdlib.h>
 #define endl '\n'
 
 using namespace std;
 
-bool anagramCheck(string,string,char);
+bool anagramWordCheck(string,string,char);
+bool anagramSentenceCheck(string,string,char);
 
 int main()
 {
@@ -21,15 +23,29 @@ int main()
         cout<<"Please type 's' for Case Sensitive Anagram Checking or 'i' for Case Insensitive Anagram Checking: ";
         cin>>sensitivity;
 
-        cout<<(anagramCheck(word1, word2,tolower(sensitivity))?"The two words are anagrams.":"The two words are not anagrams.");
+        cout<<(anagramWordCheck(word1, word2, tolower(sensitivity))?"The two words are anagrams.":"The two words are not anagrams.");
+    }
 
+    else if(choice=='s' || choice=='S')
+    {
+        string sentence1, sentence2;
+        cout<<"Please enter the first sentence:\n";
+        cin.ignore();
+        getline(cin,sentence1);
+        cout<<"Please enter the second sentence:\n";
+        getline(cin,sentence2);
+
+        cout<<"Please type 's' for Case Sensitive Anagram Checking or 'i' for Case Insensitive Anagram Checking: ";
+        cin>>sensitivity;
+
+        cout<<(anagramSentenceCheck(sentence1, sentence2, tolower(sensitivity))?"The two sentences are anagrams.":"The two sentences are not anagrams.");
     }
 
     return 0;
 }
 
 
-bool anagramCheck(string word1, string word2, char sensitivity)
+bool anagramWordCheck(string word1, string word2, char sensitivity)
 {
     if(word1.length()!=word2.length())
         return false;
@@ -73,4 +89,46 @@ bool anagramCheck(string word1, string word2, char sensitivity)
     }
 
     return(differentOrder&&sameFrequency?true:false);
+}
+
+//space separated words
+bool anagramSentenceCheck(string sentence1, string sentence2, char sensitivity)
+{
+    if(sentence1.size()!=sentence2.size())
+        return false;
+
+    vector<pair<string,bool>> sentence1_words;
+    vector<string> sentence2_words;
+    string word;
+    stringstream ss1(sentence1);
+    while(getline(ss1,word,' '))
+    {
+        sentence1_words.push_back(make_pair(word,false));
+    }
+
+    stringstream ss2(sentence2);
+    while(getline(ss2,word,' '))
+    {
+        sentence2_words.push_back(word);
+    }
+
+    if(sentence1_words.size()!=sentence2_words.size())
+        return false;
+
+    int i, j, number_of_words=sentence1_words.size(), anagramCount=0;
+    for(i=0; i<number_of_words; i++)
+    {
+        for(j=0; j<number_of_words; j++)
+        {
+            if(sentence1_words[j].second==true)
+                continue;
+            if(anagramWordCheck(sentence1_words[j].first,sentence2_words[i],sensitivity))
+            {
+                anagramCount++;
+                sentence1_words[j].second=true;
+                break;
+            }
+        }
+    }
+    return(anagramCount==number_of_words?true:false);
 }
