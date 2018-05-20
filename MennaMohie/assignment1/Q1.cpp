87ec375b18a3c4a6fbce481f9de2a1f5211297bb
MennaMohie <mennaxmohiex@gmail.com>
2018-05-20 19:56:13 +0200

    Added Case Sensitive and Case Insensitive options

--- MennaMohie/assignment1/Q1.cpp
+++ MennaMohie/assignment1/Q1.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 
-bool anagramCheck(string,string);
+bool anagramCheck(string,string,char);
 
 int main()
 {
-    char choice;
+    char choice, sensitivity;
     cout<<"Please type 'w' if you'd like to enter 2 words or 's' if you'd like to enter 2 sentences: ";
     cin>>choice;
     if(choice=='w' || choice=='W')
@@ -18,7 +18,10 @@ int main()
         cout<<"Please enter the second word: ";
         cin>>word2;
 
-        cout<<(anagramCheck(word1, word2)?"The two words are anagrams.":"The two words are not anagrams.");
+        cout<<"Please type 's' for Case Sensitive Anagram Checking or 'i' for Case Insensitive Anagram Checking: ";
+        cin>>sensitivity;
+
+        cout<<(anagramCheck(word1, word2,tolower(sensitivity))?"The two words are anagrams.":"The two words are not anagrams.");
 
     }
 
@@ -26,7 +29,7 @@ int main()
 }
 
 
-bool anagramCheck(string word1, string word2)
+bool anagramCheck(string word1, string word2, char sensitivity)
 {
     if(word1.length()!=word2.length())
         return false;
@@ -37,10 +40,12 @@ bool anagramCheck(string word1, string word2)
     int length = word1.size();
     for(i=0; i<length; i++)
     {
+        if(sensitivity=='i')
+        {
+            word1[i]=tolower(word1[i]);
+            word2[i]=tolower(word2[i]);
+        }
         word1_map[word1[i]].push_back(i);
-    }
-    for(i=0; i<length; i++)
-    {
         word2_map[word2[i]].push_back(i);
     }
 
