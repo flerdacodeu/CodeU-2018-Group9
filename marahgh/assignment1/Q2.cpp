507c30d47ecadbf4cfe996d1a48365f3a26ead7c
marahgh <38587935+marahgh@users.noreply.github.com>
2018-05-26 23:12:14 +0300

    Update Q2.cpp

--- marahgh/assignment1/Q2.cpp
+++ marahgh/assignment1/Q2.cpp
@@ -1,38 +1,27 @@
 
+
 #include <iostream>
 #include <ext/slist>
 
 using namespace std;
 
 
-/*Node's list class */
-template <typename T>
-class Node{
-    T data;
-    Node* next;
-    public:
-    Node(T data):data(data),next(nullptr){
-    }
-    Node* getNext(){
-       return next; 
-    }
-    T& getData(){
-        return data;
-    }
-	void setData(T& data){
-		data=data;
-	}
-	void setNext(Node* node){
-		next=node;
-	}
-	
-};
 //----------------------------------------------------------------------------------//
 /* A generic list with basic functionality, constructor, destructor,
     insertion to the end if the list and removing the last element of the list*/
 template< typename T>
 class List{
-    
+/*Node's list class */
+template <typename S>
+class Node{
+    public:
+    S data;
+    Node<S>* next;
+    public:
+    Node(S data):data(data),next(nullptr){
+    }
+};    
+	private:
 	Node<T>* head;
 	Node<T>* tail;
 	int size;
@@ -42,29 +31,23 @@ class List{
 	List():head(nullptr),tail(nullptr),size(0){}
 	
 	Node<T>* getHead(){
-	    if(!this){
-	       return nullptr;
-	    }
 	    return head;
 	}
 	
 	int getSize(){
-	    if(!this){
-	        return -1;
-	    }
         return size;
     }
 	
 	void insertLast(T& data){
 	    Node<T>* nodeToInsert = new Node<T>(data);
-	    nodeToInsert->setNext(nullptr);
+	    nodeToInsert->next=nullptr;
 		if(!head){
 			head=nodeToInsert;
 			tail=nodeToInsert;
 			size=1;
 			return;
 		}
-		tail->setNext(nodeToInsert);
+		tail->next=nodeToInsert;
 		tail=nodeToInsert;
 		size++;
 	}
@@ -74,8 +57,7 @@ class List{
             return;
         }
         Node<T>* curr=head;
-        Node<T>* beforeLast=nullptr;
-        if(curr->getNext()==nullptr){
+        if(curr->next==nullptr){
             delete head;
             head=nullptr;
             tail=nullptr;
@@ -83,15 +65,14 @@ class List{
             return;
         }
         while (curr){
-            if(curr->getNext()== tail){
-                beforeLast=curr;
+            if(curr->next== tail){
                 break;
             }
-            curr=curr->getNext();
+            curr=curr->next;
         }
         delete tail;
-        tail=beforeLast;
-        beforeLast->setNext(nullptr);
+        tail=curr;
+        curr->next=nullptr;
         size--;
     }
     
@@ -101,28 +82,26 @@ class List{
 	    }
 	    Node<T>* currentNode = this->head; 
         while (currentNode){
-            Node<T>* nextNode = currentNode->getNext();    
+            Node<T>* nextNode = currentNode->next;    
             delete currentNode;                         
             currentNode = nextNode;    
         }
     }
-};
-
 //----------------------------------------------------------------------------------//
-/* A function to return the kth to last element of a singly linked list*/
-template<typename T>
-T* getKthToLast(List<T>& list, int k){
-    if(!list.getHead()){
+/* A Method to return the kth to last element of a singly linked list*/
+
+T* getKthToLast(int k){
+    if(!head){
         return nullptr;
     }
-    int listSize=list.getSize();
-    if(k<0 || k>=listSize){
+    if(k<0 || k>=size){
         return nullptr; 
     }
-    Node<T>* tmp=list.getHead();
-    for(int i=0;i<listSize-k-1;i++){
-        tmp=tmp->getNext();
+    Node<T>* tmp=head;
+    for(int i=0;i<size-k-1;i++){
+        tmp=tmp->next;
     }
-    return &(tmp->getData());
-}
-
+    return &(tmp->data);
+}    
+    
+};
