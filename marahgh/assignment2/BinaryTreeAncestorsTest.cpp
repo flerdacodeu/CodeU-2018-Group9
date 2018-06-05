ad4f058a9665f46729cf6f7c4f6e3dbe1b9f53fe
marahgh <38587935+marahgh@users.noreply.github.com>
2018-06-05 22:14:50 +0300

    Update BinaryTreeAncestorsTest.cpp

--- marahgh/assignment2/BinaryTreeAncestorsTest.cpp
+++ marahgh/assignment2/BinaryTreeAncestorsTest.cpp
@@ -1,5 +1,11 @@
 #include "BinaryTreeAncestors.h"
 
+void printAncestorVector(vector<int> ancestors) {
+  for (int a : ancestors) {
+    cout << a << " ";
+  }
+  cout << endl;
+}
 void tree1Test() {
   /*Building the binary tree - tree1 in the assignment example */
   /*
@@ -12,25 +18,25 @@ void tree1Test() {
       1   6
   */
   BinaryTree<int> tree1 = BinaryTree<int>(7);
-  tree1.insert(4, 7, "right");
-  tree1.insert(3, 7, "left");
-  tree1.insert(8, 4, "right");
-  tree1.insert(5, 3, "right");
-  tree1.insert(2, 3, "left");
-  tree1.insert(1, 2, "left");
-  tree1.insert(6, 2, "right");
+  tree1.insert(4, 7, Right);
+  tree1.insert(3, 7, Left);
+  tree1.insert(8, 4, Right);
+  tree1.insert(5, 3, Right);
+  tree1.insert(2, 3, Left);
+  tree1.insert(1, 2, Left);
+  tree1.insert(6, 2, Right);
   cout << "--- tree1 testing ---" << endl;
-  tree1.printAncestors(1);  // must print 2 3 7
+  printAncestorVector(tree1.getAncestors(1));  // must print 2 3 7
   cout << "---" << endl;
-  tree1.printAncestors(6);  // must print 2 3 7
+  printAncestorVector(tree1.getAncestors(6));  // must print 2 3 7
   cout << "---" << endl;
-  tree1.printAncestors(7);  // must print nothing
+  printAncestorVector(tree1.getAncestors(7));  // must print nothing
   cout << "---" << endl;
-  tree1.printAncestors(8);  // must print 4 7
+  printAncestorVector(tree1.getAncestors(8));  // must print 4 7
   cout << "---" << endl;
-  tree1.printLowestCommonAncestor(4, 8);  // must print 4
+  printAncestorVector(tree1.lowestCommonAncestor(4, 8));  // must print 4
   cout << "---" << endl;
-  tree1.printLowestCommonAncestor(1, 8);  // must print 7
+  printAncestorVector(tree1.lowestCommonAncestor(1, 8));  // must print 7
   cout << "---" << endl;
 }
 
@@ -48,30 +54,30 @@ void tree2Test() {
          9  5
   */
   BinaryTree<int> tree2 = BinaryTree<int>(3);
-  tree2.insert(8, 3, "right");
-  tree2.insert(6, 3, "left");
-  tree2.insert(11, 6, "right");
-  tree2.insert(2, 6, "left");
-  tree2.insert(5, 11, "right");
-  tree2.insert(9, 11, "left");
-  tree2.insert(13, 8, "right");
-  tree2.insert(7, 13, "right");
+  tree2.insert(8, 3, Right);
+  tree2.insert(6, 3, Left);
+  tree2.insert(11, 6, Right);
+  tree2.insert(2, 6, Left);
+  tree2.insert(5, 11, Right);
+  tree2.insert(9, 11, Left);
+  tree2.insert(13, 8, Right);
+  tree2.insert(7, 13, Right);
   cout << "--- tree2 testing ---" << endl;
-  tree2.printAncestors(9);  // must print 11 6 3
+  printAncestorVector(tree2.getAncestors(9));  // must print 11 6 3
   cout << "---" << endl;
-  tree2.printAncestors(6);  // must print 3
+  printAncestorVector(tree2.getAncestors(6));  // must print 3
   cout << "---" << endl;
-  tree2.printAncestors(13);  // must print 8 3
+  printAncestorVector(tree2.getAncestors(13));  // must print 8 3
   cout << "---" << endl;
-  tree2.printAncestors(5);  // must print 11 6 3
+  printAncestorVector(tree2.getAncestors(5));  // must print 11 6 3
   cout << "---" << endl;
-  tree2.printLowestCommonAncestor(2, 5);  // must print 6
+  printAncestorVector(tree2.lowestCommonAncestor(2, 5));  // must print 6
   cout << "---" << endl;
-  tree2.printLowestCommonAncestor(7, 9);  // must print 3
+  printAncestorVector(tree2.lowestCommonAncestor(7, 9));  // must print 3
   cout << "---" << endl;
-  tree2.printLowestCommonAncestor(7, 8);  // must print 8
+  printAncestorVector(tree2.lowestCommonAncestor(7, 8));  // must print 8
   cout << "---" << endl;
-  tree2.printLowestCommonAncestor(5, 13);  // must print 3
+  printAncestorVector(tree2.lowestCommonAncestor(5, 13));  // must print 3
   cout << "---" << endl;
 }
 
