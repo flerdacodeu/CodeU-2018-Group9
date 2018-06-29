bf3dfa21d4cf02b9f642955c8204d2666cdf6b1f
Laia Freixas <laiafreixas9@gmail.com>
2018-06-29 13:37:43 +0200

    added comments and change a return type

--- laiaaa0/assignment4/Map.cpp
+++ laiaaa0/assignment4/Map.cpp
@@ -3,10 +3,10 @@
 int directions_x [] = {0,0,-1,1};
 int directions_y [] = {1,-1,0,0};
 
-Map::Map(){
+Map::Map() {
 
 }
-bool Map::ReadMap(){
+bool Map::ReadMap() {
     bool return_value = true;
     this->map_tiles_.clear();
     std::cin>>this->num_rows_>>this->num_columns_;
@@ -21,7 +21,7 @@ bool Map::ReadMap(){
 
     return return_value;
 }
-void Map::WriteMap(){
+void Map::WriteMap() {
     for (int i = 0; i < this->num_rows_; i++){
         for (int j = 0; j < this->num_columns_; j++){
             std::cout<<this->map_tiles_[i][j]<<" ";
@@ -39,7 +39,7 @@ bool Map::IsInMap(int x, int y) {
 }
 
 
-bool Map::ExpandNode (int x, int y, std::vector<std::vector<bool> > & visited){
+bool Map::ExpandNode(int x, int y, std::vector<std::vector<bool> > & visited) {
     if (!IsInMap (x,y)) return false;
     if (visited[x][y]) return false;
     visited[x][y] = true;
@@ -51,34 +51,36 @@ bool Map::ExpandNode (int x, int y, std::vector<std::vector<bool> > & visited){
                 int next_x = x + directions_x[i];
                 int next_y = y + directions_y[i];
                 ExpandNode(next_x, next_y, visited);
-                
         }
         return true;
     }
 }
 
-void GetNextUnvisited (int & x, int & y, const std::vector<std::vector<bool> > & visited){
+std::pair<int,int> Map::GetNextUnvisited(int x, int y, const std::vector<std::vector<bool> > & visited) {
+    std::pair<int,int> next_unvisited;
     for (int i = 0; i < visited.size(); ++i) {
-        for (int j = 0; j< visited[i].size(); ++j){
+        for (int j = 0; j < visited[i].size(); ++j){
             if (!visited[i][j]){
-                x = i;
-                y = j;
-                return;
+                next_unvisited.first = i;
+                next_unvisited.second = j;
+                return next_unvisited;
             }
         }
     }
-    x = visited.size();
-    y = visited[0].size();
-    return;
-
+    next_unvisited.first = this->num_rows_;
+    next_unvisited.second = this->num_columns_;
+    return next_unvisited;
 } 
-int Map::FindNumberIslands(){
+
+int Map::FindNumberIslands() { 
     std::vector<std::vector<bool> > visited(this->num_rows_, std::vector<bool> (this->num_columns_,false));
-    int x = 0,y = 0;
+    int x = 0, y = 0;
     int num_islands = 0;
-    while (x<visited.size() && y < visited[x].size()){
+    while (x<this->num_rows_ && y < this->num_columns_){
         if (this->ExpandNode (x,y,visited)) num_islands++;
-        GetNextUnvisited (x,y,visited);       
+        std::pair<int,int> next_unvisited = GetNextUnvisited (x,y,visited);       
+        x = next_unvisited.first;
+        y = next_unvisited.second;
     }
     return num_islands;
 }
