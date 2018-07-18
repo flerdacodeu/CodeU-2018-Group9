285448ea250f99652d405c16a072354580c86743
MennaMohie <mennaxmohiex@gmail.com>
2018-07-18 21:40:27 +0200

    Update Solution.cpp
    I tried using a separate class for the graph, but then I realized that the AlphabetFinder class is basically a graph class, it has: the graph nodes, the graph, and the topological sorting function(s). It's just not a "general" graph class, and even if I had used a separate class for the graph, it wouldn't have been general either, because the way the graph is built is specific to the assignment's problem.
    I've tried making the code a bit clearer.
    If anyone wants to edit this, or use a separate graph for the class, they're welcome to. :)

--- group/assignment5/Solution.cpp
+++ group/assignment5/Solution.cpp
@@ -5,7 +5,8 @@
 #include<map>
 #include<unordered_map>
 #include<unordered_set>
-#include"Testing.h"
+#include"TestingMacros.h"
+
 using namespace std;
 
 class AlphabetFinder
@@ -14,17 +15,19 @@ class AlphabetFinder
 	vector<char> alphabet;
 	bool foundAlphabet = false;
 
+	//this set can be thought of as the set of IDs of the graph nodes
 	unordered_set<char> allChars;
 
-	//map value: set of chars that are lexicographically AFTER the key char
+	//the actual graph, map value: set of chars that are lexicographically AFTER the key char
 	unordered_map<char, unordered_set<char>> charsMap;
 
 	//for the topological sorting
 	stack<char> alphabetStack;
 	map<char, bool> visited;
 
-	void findAllChars()
+	void buildGraph()
 	{
+		//getting all graph nodes (all unique chars)
 		for (string word : words)
 		{
 			for (char c : word)
@@ -32,36 +35,66 @@ class AlphabetFinder
 				allChars.emplace(c);
 			}
 		}
-	}
 
-	void fillMap()
-	{
-		//check every pair of adjacent words,
+		//building the graph,
+		//for every pair of adjacent words,
 		//go through them simultaneously char by char till reaching the end of any of the 2 words,
-		//find only the first mismatching chars (if there is any), 
-		//add to the charsMap.
-		int firstLoopIterations = words.size() - 1;
+		//find only the first mismatching chars (if there are any), 
+		//map[mismatching letter from the first word] = mismatching letter from the second word,
+		//because the letter from the second word should come AFTER the one from the first word
+		int firstLoopIterations = words.size() - 1; //if we have 1 2 3 words, the pairs are: 1 2, 2 3, so 3 words -> 2 pairs
 		for (int i = 0; i < firstLoopIterations; i++)
 		{
 			string word1 = words[i];
 			string word2 = words[i + 1];
-			int secondLoopIterations = min(word1.length(), word2.length());
+			int secondLoopIterations = min(word1.length(), word2.length());//we'll stop if the end of any of the 2 words is reached
 			for (int j = 0; j < secondLoopIterations; j++)
 			{
 				if (word1[j] != word2[j])
 				{
 					charsMap[word1[j]].emplace(word2[j]);
-					break;
+					break; //we shouldn't check the rest of the 2 words, so we should move on to another pair
 				}
 			}
 		}
+	}
+
+	void topologicalSort()
+	{
+		//filling the visited map
+		unordered_set<char>::iterator it;
+		for (it = allChars.begin(); it != allChars.end(); it++)
+		{
+			visited[*it] = false;
+		}
+
+		//iterating over all chars to visit them
+		for (it = allChars.begin(); it != allChars.end(); it++)
+		{
+			if (!visited[*it])
+			{
+				visit(*it);
+			}
+		}
+
+		//when the topoligical sorting is done, that's when the alphabet's ready
+		foundAlphabet = true;
 	}
 
 	void visit(char c)
 	{
 		visited[c] = true;
 
-		//iterating over all chars that should come AFTER c
+		//iterating over all chars that should come AFTER char c,
+		//when a char is visited,
+		//all the chars that should come after it are added to the stack before it,
+		//so if we have a<b & b<c,
+		//when a is visited, it's not added to the stack, instead, it calls visit(b)
+		//when b is visited, it's not added to the stack, instead, it calls visit(c)
+		//when c is visited, nothing comes after it, so it's added to the stack, 
+		//then we go back to b, that called visit(c), and b is added to the stack,
+		//then we go back to a, that called visit(b), and a is added to the stack,
+		//when we empty the stack, they come out in this order: a b c, which is the correct order
 		unordered_set<char>::iterator it;
 		for (it = charsMap[c].begin(); it!=charsMap[c].end(); it++)
 		{
@@ -75,9 +108,9 @@ class AlphabetFinder
 	}
 
 public:
+
 	AlphabetFinder(vector<string> w) :words(w) {
-		findAllChars();
-		fillMap();
+		buildGraph();
 	}
 
 	vector<char> getAlphabet()
@@ -86,23 +119,7 @@ public:
 			return alphabet;
 		}
 
-		//filling the visited map
-		unordered_set<char>::iterator it;
-		for (it = allChars.begin(); it != allChars.end(); it++)
-		{
-			visited[*it] = false;
-		}
-
-		//iterating over all chars
-		for (it = allChars.begin(); it!=allChars.end(); it++)
-		{
-			if (!visited[*it])
-			{
-				visit(*it);
-			}
-		}
-
-		foundAlphabet = true;
+		topologicalSort();
 
 		//filling the alphabet vector
 		while (!alphabetStack.empty())
@@ -150,7 +167,7 @@ void minCapTest()
 void symbolsTest()
 {
 	AlphabetFinder AF({ "*&^", "*&%", "%%%", "&**", "&*@" });
-	vector<char> expected = { '^', '*', '@', '%', '&'};
+	vector<char> expected = { '^', '*', '@', '%', '&' };
 	vector<char> actual = AF.getAlphabet();
 	EXPECT_EQ(expected, actual);
 }
@@ -167,6 +184,6 @@ void runAllTests()
 int main()
 {
 	runAllTests();
-  
+	
 	return 0;
 }
