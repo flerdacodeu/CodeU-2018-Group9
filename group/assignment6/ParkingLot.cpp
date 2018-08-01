4d17f89d7c47c5477e085309ec41460a27711ccc
marahgh <38587935+marahgh@users.noreply.github.com>
2018-08-01 10:39:14 +0300

    Add files via upload

---
+++ group/assignment6/ParkingLot.cpp
@@ -0,0 +1,95 @@
+
+#include"ParkingLot.h"
+ParkingLot::ParkingLot(int size) :
+		cars(vector<int>()), Ps(vector<int>(size, EMPTYPS)), emptyPs(-1), carsNum(
+				0), size(size) {
+}
+
+bool ParkingLot::addCar(int ps) {
+	if (isOutOfBounds(ps)) {
+		return false;
+	}
+	if (Ps[ps] != EMPTYPS) {
+		return false;
+	}
+	if (isFull()) {
+		return false;
+	}
+	Ps[ps] = carsNum;
+	cars[carsNum] = ps;
+	carsNum++;
+	if (isFull()) {
+		updateEmptyPs();
+	}
+	return true;
+}
+
+bool ParkingLot::isOutOfBounds(int ps) {
+	return (ps < 0 || ps >= size);
+}
+
+bool ParkingLot::invalidCarNumber(int carNum) {
+	return (carNum < 0 || (unsigned int) carNum >= cars.size());
+}
+
+void ParkingLot::updateEmptyPs() {
+	if (!isFull()) {
+		return;
+	}
+	for (unsigned int i = 0; i < Ps.size(); i++) {
+		if (Ps[i] == -1) {
+			emptyPs = i;
+			return;
+		}
+	}
+}
+
+bool ParkingLot::isFull() {
+	return ((unsigned int) carsNum == Ps.size() - 1);
+}
+
+int ParkingLot::getCarAt(int ps) {
+	if (isOutOfBounds(ps)) {
+		return ERROR;
+	}
+	return Ps[ps];
+}
+
+int ParkingLot::getCarPs(int carNum) {
+	if (invalidCarNumber(carNum)) {
+		return ERROR;
+	}
+	return cars[carNum];
+}
+
+int ParkingLot::getEmptyPs() {
+	if (!isFull()) {
+		return ERROR;
+	}
+	return emptyPs;
+}
+
+bool ParkingLot::makeMove(Move move) {
+	int from = move.getFromPs();
+	int to = move.getToPs();
+	int car = move.getCar();
+	if (isOutOfBounds(from) || isOutOfBounds(to)) {
+		return false;
+	}
+	if (Ps[from] == EMPTYPS || invalidCarNumber(Ps[from])) {
+		return false;
+	}
+	if (invalidCarNumber(car)) {
+		return false;
+	}
+	if (Ps[to] != EMPTYPS) {
+		return false;
+	}
+	if (Ps[from] != car) {
+		return false;
+	}
+	Ps[to] = car;
+	cars[car] = to;
+	updateEmptyPs();
+	return true;
+}
---
+++ group/assignment6/ParkingLot.h
@@ -0,0 +1,76 @@
+
+#ifndef PARKINGLOT_H_
+#define PARKINGLOT_H_
+#include <iostream>
+using namespace std;
+#include <vector>
+#include "Move.h"
+
+/*Special Messages have negative values as follows:*/
+/*relates to an empty ps */
+#define EMPTYPS -1
+/*relates to an error that prevents doing any action*/
+#define ERROR -2
+
+/*ParkingLot class :
+ * let n be the size of the parking lot, then:
+ * n-1 cars numbers are {0,1,...,n-2},
+ * n parking spaces numbers are {0,1,..., n-1}.
+ * Special messages have negative values as described above*/
+class ParkingLot {
+
+	/*cars[i]= the ps that car no.i is placed in*/
+	vector<int> cars;
+	/*Ps[i]= the car placed in Ps no.i*/
+	vector<int> Ps;
+	/*the number if the empty ps after the parking lot is full, otherwise -1*/
+	int emptyPs;
+	/*the number of cars in the parking lot*/
+	int carsNum;
+	/*the parking lot size- number of parking spaces*/
+	int size;
+
+	/*Helping private methods*/
+	/*updates EmptyPs to the right value */
+	void updateEmptyPs();
+	/*returns a boolean value:
+	 * true- if the given ps is out of the parkingLot range
+	 * false- otherwise.*/
+	bool isOutOfBounds(int ps);
+	/*returns a boolean value:
+	 * true- if carNum is an invalid value,
+	 * false- otherwise*/
+	bool invalidCarNumber(int carNum);
+
+public:
+	/*ParkingLot constructor:
+	 * initializes an empty parking lot with size parking spaces.*/
+	explicit ParkingLot(int size);
+	/*return values:
+	 * true- if the car has been added successfully
+	 * false- otherwise.*/
+	bool addCar(int ps);
+	/*return values:
+	 * true- if the parking lot is full
+	 * false- otherwise*/
+	bool isFull();
+	/*return values:
+	 * ERROR- if the given ps is out of bounds
+	 * EMPTYPS- if the given ps is empty
+	 * otherwise- the number of the car in ps*/
+	int getCarAt(int ps);
+	/*return values:
+	 * ERROR- if the given carNum is invalid
+	 * otherwise- the number of the ps that carNum is placed in*/
+	int getCarPs(int carNum);
+	/*returns the number of the empty ps if the parking lot is full,
+	 * ERROR- otherwise */
+	int getEmptyPs();
+	/*moves a car from a ps to another, return values:
+	 * true- if the car has been moved successfully
+	 * false otherwise*/
+	bool makeMove(Move move);
+
+};
+
+#endif /* PARKINGLOT_H_ */
