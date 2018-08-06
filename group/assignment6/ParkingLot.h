692bb3a11af8d91817b1420b13c70a88759c2dae
MennaMohie <mennaxmohiex@gmail.com>
2018-08-07 00:48:02 +0100

    Update ParkingLot.h

--- group/assignment6/ParkingLot.h
+++ group/assignment6/ParkingLot.h
@@ -1,10 +1,17 @@
+/*
+* ParkingLot.h
+*
+*  Created on: Jul 31, 2018
+*      Author: Marah Ghoummaid
+*/
 
 #ifndef PARKINGLOT_H_
 #define PARKINGLOT_H_
 #include <iostream>
-using namespace std;
 #include <vector>
 #include "Move.h"
+
+using namespace std;
 
 /*Special Messages have negative values as follows:*/
 /*relates to an empty ps */
@@ -13,10 +20,10 @@ using namespace std;
 #define ERROR -2
 
 /*ParkingLot class :
- * let n be the size of the parking lot, then:
- * n-1 cars numbers are {0,1,...,n-2},
- * n parking spaces numbers are {0,1,..., n-1}.
- * Special messages have negative values as described above*/
+* let n be the size of the parking lot, then:
+* n-1 cars numbers are {0,1,...,n-2},
+* n parking spaces numbers are {0,1,..., n-1}.
+* Special messages have negative values as described above*/
 class ParkingLot {
 
 	/*cars[i]= the ps that car no.i is placed in*/
@@ -34,43 +41,45 @@ class ParkingLot {
 	/*updates EmptyPs to the right value */
 	void updateEmptyPs();
 	/*returns a boolean value:
-	 * true- if the given ps is out of the parkingLot range
-	 * false- otherwise.*/
+	* true- if the given ps is out of the parkingLot range
+	* false- otherwise.*/
 	bool isOutOfBounds(int ps);
 	/*returns a boolean value:
-	 * true- if carNum is an invalid value,
-	 * false- otherwise*/
+	* true- if carNum is an invalid value,
+	* false- otherwise*/
 	bool invalidCarNumber(int carNum);
 
 public:
 	/*ParkingLot constructor:
-	 * initializes an empty parking lot with size parking spaces.*/
+	* initializes an empty parking lot with size parking spaces.*/
 	explicit ParkingLot(int size);
+	
+	bool operator !=(const ParkingLot&);
+
 	/*return values:
-	 * true- if the car has been added successfully
-	 * false- otherwise.*/
+	* true- if the car has been added successfully
+	* false- otherwise.*/
 	bool addCar(int ps);
 	/*return values:
-	 * true- if the parking lot is full
-	 * false- otherwise*/
+	* true- if the parking lot is full
+	* false- otherwise*/
 	bool isFull();
 	/*return values:
-	 * ERROR- if the given ps is out of bounds
-	 * EMPTYPS- if the given ps is empty
-	 * otherwise- the number of the car in ps*/
+	* ERROR- if the given ps is out of bounds
+	* EMPTYPS- if the given ps is empty
+	* otherwise- the number of the car in ps*/
 	int getCarAt(int ps);
 	/*return values:
-	 * ERROR- if the given carNum is invalid
-	 * otherwise- the number of the ps that carNum is placed in*/
+	* ERROR- if the given carNum is invalid
+	* otherwise- the number of the ps that carNum is placed in*/
 	int getCarPs(int carNum);
-	/*returns the number of the empty ps if the parking lot is full,
-	 * ERROR- otherwise */
+	/*returns a vector with indices of  the empty ps in the parking lot*/
 	int getEmptyPs();
 	/*moves a car from a ps to another, return values:
-	 * true- if the car has been moved successfully
-	 * false otherwise*/
+	* true- if the car has been moved successfully
+	* false otherwise*/
 	bool makeMove(Move move);
-
+	int getSize();
 };
 
 #endif /* PARKINGLOT_H_ */
