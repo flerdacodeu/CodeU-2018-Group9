973ef877abaa865074675c2f3e5e978a57f0db03
JoodHajeer <36791021+JoodHajeer@users.noreply.github.com>
2018-08-07 00:37:33 +0300

    Update RearrangingCars.cpp

--- group/assignment6/RearrangingCars.cpp
+++ group/assignment6/RearrangingCars.cpp
@@ -8,69 +8,25 @@ using namespace std;
 RearrangingCars(const ParkingLot& initialState_){
 	initialState = initialState_;
 }
-void getAllConfigurations( ParkingLot& initialState, ParkingLot& finalState, vector<Move> configuration, vector<vector<Move>> *allConfigurations) {
-
-	if (initialState == finalState) {
-		(*allConfigurations).push_back(configuration);
-		return;
-	}
-	/*INCOMPLETE: add size() method to ParkingLot Class*/
-	for(int psID = 0; psID < initialState.size(); psID++) { // loop on initial parking lot state
-		if(initialState.getCarAt(psID) != finalState.getCarAt(psID)) { // car not in the right position
-			if(initialState.getCarAt(psID) == ParkingLot::EMPTYPS) { // empty slot
-				// make move
-				Move move = new Move(finalState.getCarAt(psID), initialState.getCarPs(finalState.getCarAt(psID)), psID); 
-				initialState.makeMove(move); 
-				configuration.push_back(move);
-				// recurse to find all possible configurations
-				getAllConfigurations(initialState, finalState, configuration, allConfigurations);
-				// undo the move for backtracking
-				configuration.pop_back();
-				initialState.undoMove(move); /*INCOMPLETE: add undoMove method to ParkingLot Class*/
-			}
-			else { 
-				/*INCOMPLETE: add getAllEmptyPs method to ParkingLot Class*/
-				vector<int> emptyPs = initialState.getAllEmptyPs(); // get all empty slots in intitial state
-				for(int emptyPsID : emptyPS) { // loop on them
-					// move the required car in the empty slot
-					Move move = new Move(initialState.getCarAt(psID), psID, emptyPsID);
-					/*INCOMPLETE: update emptyPS vector*/
-					initialState.makeMove(move);
-					configuration.push_back(move);
-					// recurse to find all possible configurations
-					getAllConfigurations(initialState, finalState, configuration, allConfigurations);
-					// undo the move for backtracking
-					configuration.pop_back();
-					initialState.undoMove(move); /*INCOMPLETE: add undoMove method to ParkingLot Class*/
-				}
-			}
-		}
-	}
-}
 void getConfiguration(ParkingLot initialState, ParkingLot finalState, vector<Move> *configuration) {
-
-	if (initialState == finalState) {
-		return;
-	}
-
-	for (int psID = 0; psID < initialState.size(); psID++) { // loop on initial parking lot state
-		if (initialState.getCarAt(psID) != finalState.getCarAt(psID)) { // car not in the right position
-			if (initialState.getCarAt(psID) == ParkingLot::EMPTYPS) { // empty slot
-				// move the required car in the empty slot
-				Move move = new Move(finalState.getCarAt(psID), initialState.getCarPs(finalState.getCarAt(psID)), psID); 
-				initialState.makeMove(move);
-				(*configuration).push_back(move); // save move
-			}
-			else { 
-				/*INCOMPLETE: add getRandomEmptyPs() method to ParkingLot Class*/
-				int emptyPsID = initialState.getRandomEmptyPs(); // get any random empty slot
-				// move the car in this position to an empty slot
-				Move move = new Move(initialState.getCarAt(psID), psID, emptyPsID);
-				/*INCOMPLETE: update emptyPS vector*/
-				initialState.makeMove(move);
-				(*configuration).push_back(move); // save move
-				psID--; //now it became empty still need to move the right car
-			}
+	while(initialState != finalState){
+		while (finalState.getCarAt(emptyPsID)!=initialState.getCarAt(emptyPsID)) { 
+			// move the required car in the empty slot
+			Move move = new Move(finalState.getCarAt(emptyPsID), initialState.getCarPs(finalState.getCarAt(emptyPsID)), emptyPsID); 
+			initialState.makeMove(move);
+			(*configuration).push_back(move); // save move
+		}
+		if(initialState.getCarAt(emptyPsID) == ParkingLot::EMPTYPS && finalState.getCarAt(emptyPsID)==initialState.getCarAt(emptyPsID)) { 
+			int firstIncorrectCar;
+			for(int i = 0; i < initialState.size(); i++)
+				if(initialState.getCarAt(i) != finalState.getCarAt(i)){
+					firstIncorrectCar=i;
+					break;
+				}
+			// move the car in this position to an empty slot
+			Move move = new Move(initialState.getCarAt(firstIncorrectCar), firstIncorrectCar, emptyPsID);
+			initialState.makeMove(move);
+			(*configuration).push_back(move); // save move
 		}
 	}
 	return;
