b92735a07d72facbe0786535650167a329e2030e
MennaMohie <mennaxmohiex@gmail.com>
2018-08-07 00:52:53 +0100

    Update and rename rearrangingCars.h to RearrangingCars.h

---
+++ group/assignment6/RearrangingCars.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include "Move.h"
+#include "ParkingLot.h"
+using namespace std;
+
+//sorry for the empty class, it's a long story
+
+class RearrangingCars {
+
+public:
+	void getConfiguration(ParkingLot initialState, ParkingLot finalState, vector<Move> *configuration);
+};
